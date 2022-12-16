df12104eafd28b56b1dc556a2280573d5d5a2805
wd29group <wd29group@gmail.com>
2022-12-16 12:45:07 +0800

    123

--- src/Nopage.jsx
+++ src/Nopage.jsx
@@ -1,9 +1,12 @@
 import { Link } from "react-router-dom";
 function Nopage() {
-    return (
-        <>
-            <h1>404 Page not found...</h1>
-        </>
-    )
+  return (
+    <>
+      <center className="m-5 p-5">
+        <h1>404 Page not found...</h1>
+        <h5>Please check your url if typed correctly.</h5>
+      </center>
+    </>
+  );
 }
 export default Nopage;
